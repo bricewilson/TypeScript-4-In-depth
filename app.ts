d61b80b28a0a0731fcb8a889f703e626e4cf1600
Brice Wilson <brice@5wilsons.net>
2021-10-27 13:15:06 -0400

    recorded m4 demo 2

--- app.ts
+++ app.ts
@@ -1,9 +1,9 @@
 function GetAllBooks() {
   let books = [
-    { title: 'Ulysses', author: 'James Joyce', available: true },
-    { title: 'A Farewell to Arms', author: 'Ernest Hemingway', available: false },
-    { title: 'I Know Why the Caged Bird Sings', author: 'Maya Angelou', available: true },
-    { title: 'Moby Dick', author: 'Herman Melville', available: true }
+    { title: 'Ulysses', author: 'James Joyce', available: true, category: Category.Fiction },
+    { title: 'A Farewell to Arms', author: 'Ernest Hemingway', available: false, category: Category.Fiction },
+    { title: 'I Know Why the Caged Bird Sings', author: 'Maya Angelou', available: true, category: Category.Poetry },
+    { title: 'Moby Dick', author: 'Herman Melville', available: true, category: Category.Fiction }
   ];
   return books;
 }
@@ -23,5 +23,28 @@ function LogFirstAvailable(books): void {
   console.log('First Available: ' + firstAvailable);
 }
 
-const allBooks = GetAllBooks();
-LogFirstAvailable(allBooks);
+function GetBookTitlesByCategory(categoryFilter: Category): Array<string> {
+  console.log('Getting books in category: ' + categoryFilter);
+
+  const allBooks = GetAllBooks();
+  const filteredTitles: string[] = [];
+
+  for(let currentBook of allBooks) {
+    if(currentBook.category === categoryFilter) {
+      filteredTitles.push(currentBook.title);
+    }
+  }
+  
+  return filteredTitles;
+}
+
+function LogBookTitles(titles: string[]): void {
+  for(let title of titles) {
+    console.log(title);
+  }
+}
+
+enum Category { Biography, Poetry, Fiction, History, Children };
+
+const poetryBooks = GetBookTitlesByCategory(Category.Poetry);
+LogBookTitles(poetryBooks);
--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 function GetAllBooks() {
     let books = [
-        { title: 'Ulysses', author: 'James Joyce', available: true },
-        { title: 'A Farewell to Arms', author: 'Ernest Hemingway', available: false },
-        { title: 'I Know Why the Caged Bird Sings', author: 'Maya Angelou', available: true },
-        { title: 'Moby Dick', author: 'Herman Melville', available: true }
+        { title: 'Ulysses', author: 'James Joyce', available: true, category: Category.Fiction },
+        { title: 'A Farewell to Arms', author: 'Ernest Hemingway', available: false, category: Category.Fiction },
+        { title: 'I Know Why the Caged Bird Sings', author: 'Maya Angelou', available: true, category: Category.Poetry },
+        { title: 'Moby Dick', author: 'Herman Melville', available: true, category: Category.Fiction }
     ];
     return books;
 }
@@ -19,5 +19,30 @@ function LogFirstAvailable(books) {
     console.log('Total Books: ' + numberOfBooks);
     console.log('First Available: ' + firstAvailable);
 }
-const allBooks = GetAllBooks();
-LogFirstAvailable(allBooks);
+function GetBookTitlesByCategory(categoryFilter) {
+    console.log('Getting books in category: ' + categoryFilter);
+    const allBooks = GetAllBooks();
+    const filteredTitles = [];
+    for (let currentBook of allBooks) {
+        if (currentBook.category === categoryFilter) {
+            filteredTitles.push(currentBook.title);
+        }
+    }
+    return filteredTitles;
+}
+function LogBookTitles(titles) {
+    for (let title of titles) {
+        console.log(title);
+    }
+}
+var Category;
+(function (Category) {
+    Category[Category["Biography"] = 0] = "Biography";
+    Category[Category["Poetry"] = 1] = "Poetry";
+    Category[Category["Fiction"] = 2] = "Fiction";
+    Category[Category["History"] = 3] = "History";
+    Category[Category["Children"] = 4] = "Children";
+})(Category || (Category = {}));
+;
+const poetryBooks = GetBookTitlesByCategory(Category.Poetry);
+LogBookTitles(poetryBooks);
