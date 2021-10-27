49968a860e84d0c72fb283651e1b4834495f175f
Brice Wilson <brice@5wilsons.net>
2021-10-27 11:32:54 -0400

    recorded m4 demo 1

--- app.ts
+++ app.ts
@@ -1,3 +1,27 @@
-let greeting: string = 'Hello TypeScript in watch mode!';
+function GetAllBooks() {
+  let books = [
+    { title: 'Ulysses', author: 'James Joyce', available: true },
+    { title: 'A Farewell to Arms', author: 'Ernest Hemingway', available: false },
+    { title: 'I Know Why the Caged Bird Sings', author: 'Maya Angelou', available: true },
+    { title: 'Moby Dick', author: 'Herman Melville', available: true }
+  ];
+  return books;
+}
 
-console.log(greeting);
+function LogFirstAvailable(books): void {
+  let numberOfBooks: number = books.length;
+  let firstAvailable: string = '';
+
+  for(let currentBook of books) {
+
+    if(currentBook.available) {
+      firstAvailable = currentBook.title;
+      break;
+    }
+  }
+  console.log('Total Books: ' + numberOfBooks);
+  console.log('First Available: ' + firstAvailable);
+}
+
+const allBooks = GetAllBooks();
+LogFirstAvailable(allBooks);
--- js/app.js
+++ js/app.js
@@ -1,2 +1,23 @@
-let greeting = 'Hello TypeScript in watch mode!';
-console.log(greeting);
+function GetAllBooks() {
+    let books = [
+        { title: 'Ulysses', author: 'James Joyce', available: true },
+        { title: 'A Farewell to Arms', author: 'Ernest Hemingway', available: false },
+        { title: 'I Know Why the Caged Bird Sings', author: 'Maya Angelou', available: true },
+        { title: 'Moby Dick', author: 'Herman Melville', available: true }
+    ];
+    return books;
+}
+function LogFirstAvailable(books) {
+    let numberOfBooks = books.length;
+    let firstAvailable = '';
+    for (let currentBook of books) {
+        if (currentBook.available) {
+            firstAvailable = currentBook.title;
+            break;
+        }
+    }
+    console.log('Total Books: ' + numberOfBooks);
+    console.log('First Available: ' + firstAvailable);
+}
+const allBooks = GetAllBooks();
+LogFirstAvailable(allBooks);
